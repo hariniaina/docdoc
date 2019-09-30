905e198773004ba082649d45073a5a8eeec16db2
luc34fenos <fenos.rasolondraibe@gmail.com>
2019-09-30 15:01:45 +0200

    v5.0.1

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,23 @@ let slidesWidth = sliders.offsetWidth;
 let pWidth = p.offsetWidth;
 let pos = -15;
 let div = document.querySelectorAll('.div .slidesBox');
+let boxCenter;
 
 left.addEventListener('click', function(){
-	if( (slidesWidth + pos -100) >= pWidth ){
+	boxCenter = document.querySelectorAll('.div .slidesBoxCenter');
+	if( (slidesWidth + pos - 100) >= pWidth ){
 		pos = pos - slideWidth - 20;
 		$(sliders).animate({
 		    marginLeft: pos + 'px'
 		}, 500);
+		for(var i=(boxCenter.length - 1); i>=0; i--){
+			boxCenter[i].classList.remove('slidesBoxCenter');
+			if(boxCenter[i].classList.contains(`slidesBoxCenter${i}`)){
+				boxCenter[i].classList.remove(`slidesBoxCenter${i}`);
+				boxCenter[i].parentNode.nextElementSibling.children[0].classList.add(`slidesBoxCenter${i}`);
+			}
+			boxCenter[i].parentNode.nextElementSibling.children[0].classList.add('slidesBoxCenter');
+		}
 	}else{
 		$(sliders).animate({
 		    marginLeft: pos - 15 + 'px'
@@ -26,11 +36,20 @@ left.addEventListener('click', function(){
 });
 
 right.addEventListener('click', function(){
+	boxCenter = document.querySelectorAll('.div .slidesBoxCenter');
 	if( pos < -15 ){
 		pos = pos + slideWidth + 20;
 		$(sliders).animate({
 		    marginLeft: pos + 'px'
 		}, 500);
+		for(var i=0; i<boxCenter.length; i++){
+			boxCenter[i].classList.remove('slidesBoxCenter');
+			if(boxCenter[i].classList.contains(`slidesBoxCenter${i}`)){
+				boxCenter[i].classList.remove(`slidesBoxCenter${i}`);
+				boxCenter[i].parentNode.previousElementSibling.children[0].classList.add(`slidesBoxCenter${i}`);
+			}
+			boxCenter[i].parentNode.previousElementSibling.children[0].classList.add('slidesBoxCenter');
+		}
 	}else{
 		$(sliders).animate({
 		    marginLeft: pos + 15 + 'px'
@@ -78,4 +97,4 @@ for(var i=0; i<div.length; i++){
 			div[x].children[0].children[0].children[0].children[0].classList.add('imgNoScale');
 		});
 	})(i);
-};
+}; 
